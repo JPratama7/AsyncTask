743937d05349e3b2b9787663bf0e7a50b09030f6
Jose Chasey Pratama <josepratama080@gmail.com>
2022-12-20 10:06:19 +0700

    upload

---
+++ app/src/main/java/crot/crot/asynctask/MainActivity.java
@@ -0,0 +1,37 @@
+package crot.crot.asynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView1);
+
+            if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    public void startTask(View view) {
+        mTextView.setText(R.string.napping);
+        new TestingAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+
+
+}
